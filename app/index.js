fa4cfaaa5b0ae0ef04b063aa55227243019c8ab9
Vitor Borges <vitorbertulucci@gmail.com>
2018-01-03 02:16:30 -0200

    Adding TextInput component

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from 'react'
 import Home from './screens/Home'
 import EStyleSheet from 'react-native-extended-stylesheet'
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
-  $white: '#FFFFFF'
+  $white: '#FFFFFF',
+  $border: '#E2E2E2',
+  $inputText: '#797979',
+  $lighGray: '#F0F0F0'
 })
 
 export default () => <Home />
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   View,
   StatusBar
@@ -6,9 +6,43 @@ import {
 
 import { Container } from '../components/Container'
 import { Logo } from '../components/Logo'
-export default () => (
-  <Container>
-    <StatusBar translucent={false} barStyle='light-content' />
-    <Logo />
-  </Container>
-)
+import { InputWithButton } from '../components/TextInput'
+
+const TEMP_BASE_CURRENCY = 'R$'
+const TEMP_QUOTE_CURRENCY = 'USD'
+const TEMP_BASE_PRICE = '100'
+const TEMP_QUOTE_PRICE = '330.00'
+
+export default class Home extends Component {
+  handlPressBaseCurrency = () => {
+    console.log('press base')
+  }
+  handlPressQuoteCurrency = () => {
+    console.log('press quote')
+  }
+  handleTextChange = (text) => {
+    console.log('change text', text)
+  }
+
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle='light-content' />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlPressBaseCurrency}
+          defaultValue={TEMP_BASE_PRICE}
+          keyboardType='numeric'
+          onChangeText={this.handleTextChange}
+        />
+        <InputWithButton
+          buttonText={TEMP_QUOTE_CURRENCY}
+          onPress={this.handlPressQuoteCurrency}
+          editable={false}
+          value={TEMP_QUOTE_PRICE}
+        />
+      </Container>
+    )
+  }
+}
